8c8d00afed52e2b040a9ebc0c177a09b8e34d35c
Michael Davey <michael.davey@robotica.ml>
2023-05-14 17:41:58 +0100

    Use callback, and throttle updates to UI

--- exts/robotica.io.spacemouse/robotica/io/spacemouse/extension.py
+++ exts/robotica.io.spacemouse/robotica/io/spacemouse/extension.py
@@ -1,4 +1,5 @@
 import carb
+from datetime import datetime, timedelta
 import omni.ext
 import omni.ui as ui
 import keyboard
@@ -6,26 +7,19 @@ import mouse
 import platform
 if platform.system() == 'Windows':
     omni.kit.pipapi.install("pywinusb")
+# import pywinusb
 import spacenavigator
-import time
+
+UPDATE_TIME_MILLIS = 100
 
 
-# Functions and vars are available to other extension as usual in python: `example.python_ext.some_public_function(x)`
-def some_public_function(x: int):
-    print("[robotica.io.spacemouse] some_public_function was called with x: ", x)
-    return x ** x
-
-
-# Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
-# instantiated when extension gets enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
-# on_shutdown() is called.
 class RoboticaIoSpacemouseExtension(omni.ext.IExt):
-    # ext_id is current extension id. It can be used with extension manager to query additional information, like where
-    # this extension is located on filesystem.
+    def __init__(self):
+        self._count = 0
+        self.previous_time = None
+
     def on_startup(self, ext_id):
         print("[robotica.io.spacemouse] robotica io spacemouse startup")
-
-        self._count = 0
 
         self._window = ui.Window("Spacemouse debug", width=300, height=300)
         with self._window.frame:
@@ -36,41 +30,57 @@ class RoboticaIoSpacemouseExtension(omni.ext.IExt):
                 self._label_connected = ui.Label("")
 
         # Note1: It is possible to have multiple 3D mice connected.
-        # We should use the per-device API rather than the module API
-        # if we want to support this.
         # See: https://github.com/johnhw/pyspacenavigator/blob/master/spacenavigator.py
 
-        # Note2: spacenavigator supports a callback method being passed to open().
-        # For production, we should use that rather than our while loop and time.sleep()
-        success = spacenavigator.open()
-        if success:
-            if success.connected:
+        self._nav1 = spacenavigator.open(callback=self.on_spacemouse,button_callback=self.on_spacemouse_buttons, DeviceNumber=0)
+        self._nav2 = spacenavigator.open(callback=self.on_spacemouse,button_callback=self.on_spacemouse_buttons, DeviceNumber=1)
+
+        if self._nav1 or self._nav2:
+            if self._nav1.connected or self._nav2.connected:
                 self._label_connected.text = "Connected"
             else:
                 self._label_connected.text = "Not Connected"
-            while 1:
-                state = spacenavigator.read()
-                msg = f"{state.x}, {state.y}, {state.z}"
-                msg2 = f"roll: {state.roll}, pitch: {state.pitch}, yaw: {state.yaw}"
-                # Note1: The number of buttons varies with the type of 3DConnexion product we have
-                # Note2: The mappings of buttons is user-configurable so not guaranteed - we have to account for this
-                buttons = f"buttons: {state.button}"
-                carb.log_warn(msg)
-                self._label_msg.text = msg
-                self._label_msg2.text = msg2
-                self._label_buttons.text = buttons
-                self._label_connected.text = state.t
-
-                if False:
-                    key = "ctrl+L"
-                    keyboard.press_and_release(key)
-                    mouse.click(button=mouse.LEFT)
-
-                time.sleep(0.5)
         else:
-            msg = "No spacemouse detected"
-            carb.log_warn(msg)
-            self._label_connected.text = msg
+            self._label_connected.text = "No spacemouse detected"
+
+    def on_spacemouse(self, state: spacenavigator):
+        current_time = datetime.now()
+        if self.previous_time:
+            if current_time - self.previous_time < timedelta(milliseconds=UPDATE_TIME_MILLIS):
+                return
+            
+        self.previous_time = current_time
+        return self.update_state(state)
+
+    def on_spacemouse_buttons(self, state: spacenavigator, buttons: spacenavigator.ButtonState):
+        return self.update_state(state)
+
+    def update_state(self, state: spacenavigator):
+        msg = f"{state.x:.03f}, {state.y:.03f}, {state.z:.03f}"
+        msg2 = f"roll: {state.roll:.03f}, pitch: {state.pitch:.03f}, yaw: {state.yaw:.03f}"
+        # Note1: The number of buttons varies with the type of 3DConnexion product we have
+        # Note2: The mappings of buttons is user-configurable so not guaranteed order - we have to account for this
+        buttons = f"buttons: {state.buttons}"
+        self._label_msg.text = msg
+        self._label_msg2.text = msg2
+        self._label_buttons.text = buttons
+        self._label_connected.text = f"{state.t}"
+
 
     def on_shutdown(self):
+        if self._nav1:
+            self._nav1.close()
+        if self._nav2:
+            self._nav2.close()
+        self._nav1 = None
+        self._nav2 = None
+        self.previous_time = None
+        if self._label_msg:
+            self._label_msg.text = ""
+        if self._label_msg2:
+            self._label_msg2.text = ""
+        if self._label_buttons:
+            self._label_buttons.text = ""
+        if self._label_connected:
+            self._label_connected.text = "Not connected"
         print("[robotica.io.spacemouse] robotica io spacemouse shutdown")
